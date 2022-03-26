afedca531e0c29fa376ce76f3a1a4dc9befbe6b5
DevArtist254 <kevinwasonga415@outlook.com>
2022-03-27 01:19:51 +0300

    Console log value for inputs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 function App() {
+  const upFileLoad = (e) => {
+    console.log(e.target.value)
+  }
+
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <input type="file" onChange={upFileLoad} />
     </div>
   )
 }
